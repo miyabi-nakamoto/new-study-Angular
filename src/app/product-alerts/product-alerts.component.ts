97e5701aa0a97d846e2210680b10876b5defe2be
miyabi-nakamoto <miyabi.nakamoto13@gmail.com>
2021-10-15 13:54:20 +0900

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
 
-  @Input() name;
+  @Input() name!:string;
   
   constructor() { }
 
